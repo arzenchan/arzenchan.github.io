7c309ecd6b3cfb1fe12ccb190cc59101b9c6c1ef
arzenchan <arzenchanemail@gmail.com>
2023-09-23 01:15:53 -0400

    Front Page Draft, Resume Imbed Test, General Layout
    Lots of stuff, basically the first implementation of the site with most of the content still missing, but the BONES are there.

---
+++ sidebar.js
@@ -0,0 +1,23 @@
+//
+//Nav bar generator.Copied from Index page into a HTML to JS converter
+//
+
+document.write("<div class=\"w3-sidebar menu\" style=\"width:350px; padding:50px; border-style: none none none none; border-right-color:grey; border-right-width:20px\">");
+document.write("  <a href=\"index.html\"><h1 style=\"line-height:90%; font-size:50px;\">");
+document.write("    Arzen <br>");
+document.write("    Chan");
+document.write("  </h1></a>");
+document.write("  <h5>");
+document.write("    <b>Urban Planner</b>");
+document.write("  </h5>");
+document.write("  <h3>");
+document.write("    <a href=\"index.html\">About Me </a><br>");
+document.write("    <a href=\"resume.html\">R&#233sum&#233</a><br>");
+document.write("    <a href=\"projects.html\">Projects</a><br>");
+document.write("    <a href=\"misc.html\">Miscellanious</a><br>");
+document.write("  </h3>");
+document.write("  <p>");
+document.write("  Contact:<br>");
+document.write("  <a href = \"mailto: arzenchanemail@gmail.com\">arzenchanemail@gmail.com</a>");
+document.write("  647-890-1998</p>");
+document.write("</div>");
